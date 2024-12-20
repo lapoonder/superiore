309c0ce17d302d6affafa213780cc41b333c7e56
Volodymyr Derhilov <v.dergilov@gmail.com>
2024-12-20 23:16:29 +0200

    base marking and a query on backend

--- src/js/goit-reviewsAPI.js
+++ src/js/goit-reviewsAPI.js
@@ -1,1 +1,11 @@
 'use strict';
+
+import axios from 'axios';
+
+const BASE_URL = 'https://portfolio-js.b.goit.study/api/reviews';
+
+export default async function getReviews() {
+  const response = await axios.get(`${BASE_URL}`);
+  console.log(response.data);
+  return response.data;
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 import { message, messageInfo } from './js/iziToast.js';
+import getReviews from './js/goit-reviewsAPI.js';
+
+const cards = document.querySelector('.cards');
+
+try {
+  const result = await getReviews();
+  cards.insertAdjacentHTML('beforeend', createMarkup(result));
+} catch {}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ author, avatar_url, review }) =>
+        `<li class="review-card"><img src="${avatar_url}" class="review-card-img"><h3 class="card-title">${author}</h3><p class="card-paragraph">${review}</p></li>`
+    )
+    .join('');
+}
