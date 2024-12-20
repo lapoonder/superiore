ceb320aa8ed2817cd49060d9a7602b79e4c70090
Aleksandr Davydenko <ozziaqueen@gmail.com>
2024-12-20 18:11:03 +0100

    rep

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,4 @@
 'use strict';
 
-import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
