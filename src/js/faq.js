3a050d2b179de8df94c4f30273835c8590970dda
Bibosss <savoniksasha7@gmail.com>
2024-12-24 00:07:17 +0200

    try accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,19 +3,33 @@
 import Accordion from "./accordion.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const triggers = document.querySelectorAll('.ac-trigger');
-
-    triggers.forEach(trigger => {
-        trigger.addEventListener('click', () => {
-            const body = trigger.parentElement.nextElementSibling;
-            const iconUp = trigger.querySelector('.icon-up');
-            const iconDown = trigger.querySelector('.icon-down');
-
-            body.classList.toggle('visible');
-            trigger.classList.toggle('open');
-
-            iconUp.style.display = body.classList.contains('visible') ? "none" : "inline";
-            iconDown.style.display = body.classList.contains('visible') ? "inline" : "none";
-        });
+    const accordion = new Accordion('.accordion', {
+        duration: 400,
+        showMultiple: false,
+        collapse: true,
+        elementClass: 'accordion__item',
+        triggerClass: 'ac-trigger',
+        panelClass: 'ac-panel',
+        activeClass: 'visible',
     });
+    
+    console.log('Accordion initialized');
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const triggers = document.querySelectorAll('.ac-trigger');
+
+//     triggers.forEach(trigger => {
+//         trigger.addEventListener('click', () => {
+//             const body = trigger.parentElement.nextElementSibling;
+//             const iconUp = trigger.querySelector('.icon-up');
+//             const iconDown = trigger.querySelector('.icon-down');
+
+//             body.classList.toggle('visible');
+//             trigger.classList.toggle('open');
+
+//             iconUp.style.display = body.classList.contains('visible') ? "none" : "inline";
+//             iconDown.style.display = body.classList.contains('visible') ? "inline" : "none";
+//         });
+//     });
+// });
