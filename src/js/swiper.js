10f861523cf515cee3efb09f3633d8573e6d4dac
AlexKaplan <alexkaplan110606@gmail.com>
2024-12-23 17:16:40 +0200

    add code

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,42 +1,37 @@
 'use strict';
+import 'swiper/css';
 
 import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
-/*const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container', {
+    modules: [Navigation, Keyboard, Mousewheel],
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 0,
     loop: false,
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
     keyboard: {
         enabled: true,
         onlyInViewport: true,
     },
+
     mousewheel: true,
-    pagination: false,
-    scrollbar: false,
+
     on: {
         slideChange: function () {
-            const totalSlides = swiper.slides.length;
-            const currentSlide = swiper.activeIndex;
-
             const prevButton = document.querySelector('.swiper-button-prev');
             const nextButton = document.querySelector('.swiper-button-next');
 
-            if (currentSlide === 0) {
-                prevButton.disabled = true;
-            } else {
-                prevButton.disabled = false;
-            }
+            prevButton.disabled = swiper.isBeginning;
+            nextButton.disabled = swiper.isEnd;
 
-            if (currentSlide === totalSlides - 1) {
-                nextButton.disabled = true;
-            } else {
-                nextButton.disabled = false;
-            }
+            prevButton.classList.toggle('swiper-button-disabled', swiper.isBeginning);
+            nextButton.classList.toggle('swiper-button-disabled', swiper.isEnd);
         },
     },
-});*/
+});
