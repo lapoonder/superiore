f72de101d278ac9731dfe842c0cbb7a42dc2847b
Aleksandr Davydenko <ozziaqueen@gmail.com>
2024-12-25 23:10:31 +0100

    about me all

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,5 +1,34 @@
 'use strict';
 
 import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-export default Accordion;
+new Accordion('.accordion-list', {
+    duration: 400,
+    showMultiple: true,
+    openOnInit: [0],
+});
+
+new Accordion('.faq-accordion-container', {
+    showMultiple: true,
+});
+
+// Only for FAQ section
+
+const arrows = document.querySelectorAll('.faq-ac-trigger');
+
+for (const arrow of arrows) {
+    arrow.addEventListener('click', UpDown);
+}
+
+function UpDown(event) {
+    const arrowDown = event.currentTarget.querySelector('.faq-arrow-down');
+    const arrowUp = event.currentTarget.querySelector('.faq-arrow-up');
+    if (event.currentTarget.getAttribute('aria-expanded') === 'true') {
+        arrowDown.classList.add('visually-hidden');
+        arrowUp.classList.remove('visually-hidden');
+    } else {
+        arrowDown.classList.remove('visually-hidden');
+        arrowUp.classList.add('visually-hidden');
+    }
+}
